9bb4f008240e82d6d7c4f0fff0b5c71176b6e9c1
Cosmin Dumitrache <cosmin.dumitrache7@gmail.com>
2025-03-02 23:40:36 +0200

    Start catalog page.

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Class} from './../../model/class.model';
+import {SchoolAdminService } from './../../services/schooladmin.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
 
+
+  classes: Class[];
+  curentClass :Class;
+  public constructor(private saService: SchoolAdminService) {
+    
+
+  }
+  ngOnInit(): void {
+    this.saService.getAllClasses(1).subscribe({
+      next: (data) =>  {
+        this.classes = data;
+        this.curentClass = this.classes[0];
+        console.log('Loading catalogs');
+      },
+      error: (err) => alert('Error loading classes')
+    });
+}
 }
